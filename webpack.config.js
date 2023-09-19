ae79224a02e17e0f9fe3aceb61e22b604485ecb2
Abdulholiq13 <abdulholiq571@gmail.com>
2023-09-19 23:30:04 +0500

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'development',
+  entry: './src/js/script.js',
+  output: {
+    path: __dirname + '/dist/js',
+    filename: 'bundle.js',
+  },
+  watch: true,
+  devtool: 'source-map',
+  module: {},
+  devServer: {
+    inline: false,
+    contentBase: "./dist",
+  }
+}
